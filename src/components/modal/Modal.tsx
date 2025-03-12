25444b8dd8483c1df9cbb8c1376f50548015dd23
jinew <jiyunk@naver.com>
2025-03-13 01:21:10 +0900

    add:#3 최상위 모달 컴포넌트 생성 및 전역 상태 provider 추가

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+import { ReactNode } from 'react';
+import { ModalProvider } from './context';
+
+const Modal = ({ children }: { children: ReactNode }) => {
+  return <ModalProvider>{children}</ModalProvider>;
+};
+
+export default Modal;
