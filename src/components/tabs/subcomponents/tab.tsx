99c3b60564ca5c310ecc6c8d8b0e9e91dff54421
sohxxny <sohxxny@gmail.com>
2025-03-17 17:12:03 +0900

    fix:#13 태그 제네릭 방식으로 변경

--- src/components/tabs/subcomponents/tab.tsx
+++ src/components/tabs/subcomponents/tab.tsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { ElementType, useContext } from 'react';
 import { TabProps } from '../type';
 import { TabsContext } from '../context';
 
-const Tab = ({ children, id, as: Tag = 'button', ...props }: TabProps) => {
+const Tab = <T extends keyof HTMLElementTagNameMap = 'button'>({
+  children,
+  id,
+  as = 'button' as T,
+  ...props
+}: TabProps<T>) => {
   const { handleChange } = useContext(TabsContext);
+  const Tag = as as ElementType;
 
   if (typeof id !== 'string') {
     console.warn(`Tabs: 'id' prop should be a string, but received '${typeof id}'.`);
--- src/components/tabs/type.ts
+++ src/components/tabs/type.ts
@@ -3,7 +3,6 @@ import { ReactElement } from 'react';
 type DivProps = React.HTMLAttributes<HTMLDivElement>;
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
 type UlProps = React.HTMLAttributes<HTMLUListElement>;
-type LiProps = React.HTMLAttributes<HTMLLIElement>;
 
 export type TabsComponentProps = {
   children: [ReactElement<TabGroupProps>, ReactElement<ContentGroupProps>];
@@ -16,19 +15,16 @@ export type TabGroupProps = UlProps & {
   children: ReactElement<TabProps> | ReactElement<TabProps>[];
 };
 
-export type TabProps = LiProps &
-  ButtonProps & {
-    children?: React.ReactNode;
-    id: string;
-    as?: React.ElementType;
-  };
+export type TabProps<T extends keyof HTMLElementTagNameMap = 'button'> = ButtonProps & {
+  id: string;
+  as?: T;
+} & React.HTMLAttributes<HTMLElementTagNameMap[T]>;
 
 export type ContentGroupProps = DivProps & {
   children: ReactElement<ContentProps> | ReactElement<ContentProps>[];
 };
 
 export type ContentProps = DivProps & {
-  children?: React.ReactNode;
   id: string;
 };
 
