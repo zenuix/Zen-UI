1f644b08f627c193c610652324f9cc795d6e6512
jinew <jiyunk@naver.com>
2025-03-17 16:04:31 +0900

    fix : 사용자가 onClick이벤트 선언 시 이벤트가 덮어씌워지지 않게 수정

--- src/components/modal/subcomponents/modal-container.tsx
+++ src/components/modal/subcomponents/modal-container.tsx
@@ -4,10 +4,17 @@ import { BaseDivProps } from '../type';
 const ModalContainer = ({ children, className = '', ...restProps }: BaseDivProps) => {
   const { isOpen, isVisible } = useModal();
 
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    if (typeof restProps.onClick === 'function') {
+      restProps.onClick(e);
+    }
+  };
+
   if (!isOpen && !isVisible) return null;
 
   return (
-    <div className={`modal-container ${className}`} {...restProps} onClick={(e) => e.stopPropagation()}>
+    <div className={`modal-container ${className}`} onClick={handleClick} {...restProps}>
       {children}
     </div>
   );
