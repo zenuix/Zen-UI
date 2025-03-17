d21817cbd5cbf94232bea6f8b0a06e180ef6bd42
jinew <jiyunk@naver.com>
2025-03-17 10:55:47 +0900

    fix : overlay 추가

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,20 +1,22 @@
-import ModalComponent from './Modal';
-import Container from './subcomponents/Container';
-import Content from './subcomponents/Content';
-import Description from './subcomponents/Description';
-import Footer from './subcomponents/Footer';
-import Header from './subcomponents/Header';
-import Title from './subcomponents/Title';
-import Trigger from './subcomponents/Trigger';
+import ModalComponent from './modal';
+import ModalContainer from './subcomponents/modal-container';
+import ModalContent from './subcomponents/modal-content';
+import ModalDescription from './subcomponents/modal-description';
+import ModalFooter from './subcomponents/modal-footer';
+import ModalHeader from './subcomponents/modal-header';
+import ModalOverlay from './subcomponents/modal-overlay';
+import ModalTitle from './subcomponents/modal-title';
+import ModalTrigger from './subcomponents/modal-trigger';
 
 const Modal = Object.assign(ModalComponent, {
-  container: Container,
-  trigger: Trigger,
-  header: Header,
-  footer: Footer,
-  title: Title,
-  description: Description,
-  content: Content
+  overlay: ModalOverlay,
+  container: ModalContainer,
+  content: ModalContent,
+  trigger: ModalTrigger,
+  header: ModalHeader,
+  footer: ModalFooter,
+  title: ModalTitle,
+  description: ModalDescription
 });
 
 export default Modal;
