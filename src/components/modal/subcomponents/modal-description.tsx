bbe1dbcc45b0540914270c981c5f8e9fdb8f9ecd
jinew <jiyunk@naver.com>
2025-03-17 16:05:03 +0900

    fix : 초기값을 인자에서 명명

--- src/components/modal/subcomponents/modal-description.tsx
+++ src/components/modal/subcomponents/modal-description.tsx
@@ -1,8 +1,8 @@
 import { ElementType } from 'react';
 import { DescriptionProps, Paragraphs } from '../type';
 
-const ModalDescription = <T extends Paragraphs = 'p'>({ as, children, ...props }: DescriptionProps<T>) => {
-  const Component = (as || 'p') as ElementType;
+const ModalDescription = <T extends Paragraphs = 'p'>({ as = 'p' as T, children, ...props }: DescriptionProps<T>) => {
+  const Component = as as ElementType;
   return <Component {...props}>{children}</Component>;
 };
 
