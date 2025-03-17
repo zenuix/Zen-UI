7bfe7b867e47923e3455e4e2f9c66d0f3741225a
jinew <jiyunk@naver.com>
2025-03-17 16:03:43 +0900

    fix : 닫기 버튼 렌더링 로직의 위치를 변경하여 관심사 분리

--- src/components/modal/subcomponents/modal-close-button.tsx
+++ src/components/modal/subcomponents/modal-close-button.tsx
@@ -2,7 +2,9 @@ import { LucideX } from 'lucide-react';
 import { useModal } from '../hook';
 
 const ModalCloseButton = () => {
-  const { closeModal } = useModal();
+  const { closeModal, withCloseButton } = useModal();
+
+  if (!withCloseButton) return null;
 
   return (
     <button type="button" onClick={closeModal} className="modal-close-btn">
