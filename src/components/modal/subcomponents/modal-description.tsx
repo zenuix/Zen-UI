416f11b91f7e223b0ea72294e83144402fa2bd22
jinew <jiyunk@naver.com>
2025-03-18 17:09:53 +0900

    fix : description에서 사용하는 타입 분리

--- src/components/modal/subcomponents/modal-description.tsx
+++ src/components/modal/subcomponents/modal-description.tsx
@@ -1,5 +1,15 @@
-import { ElementType } from 'react';
-import { DescriptionProps, Paragraphs } from '../type';
+import { ElementType, ReactNode } from 'react';
+
+/** DescriptionProps - Paragraphs 범위로 제한 */
+export type Paragraphs = 'p' | 'pre' | 'div';
+export type DescriptionProps<T extends Paragraphs = 'p'> = {
+  as?: T;
+  children?: ReactNode;
+} & (T extends 'p'
+  ? React.HTMLAttributes<HTMLParagraphElement>
+  : T extends 'pre'
+  ? React.HTMLAttributes<HTMLPreElement>
+  : React.HTMLAttributes<HTMLDivElement>);
 
 const ModalDescription = <T extends Paragraphs = 'p'>({ as = 'p' as T, children, ...props }: DescriptionProps<T>) => {
   const Component = as as ElementType;
