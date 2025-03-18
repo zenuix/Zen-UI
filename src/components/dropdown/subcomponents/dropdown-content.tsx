28db7e67953d5f94e5d235d759b3eb1539efc882
llddang <bodyness@naver.com>
2025-03-19 02:56:22 +0900

    feat:#10 dropdown 메뉴 안에서 tab 이동하도록 적용

--- src/components/dropdown/subcomponents/dropdown-content.tsx
+++ src/components/dropdown/subcomponents/dropdown-content.tsx
@@ -1,17 +1,28 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { useDropdownContext } from '../hook';
 import { DropdownContentProps } from '../type';
 
 const DropdownContent = forwardRef<HTMLDivElement, DropdownContentProps>(({ children, className, ...props }, ref) => {
-  const { isOpen, triggerId, menuId } = useDropdownContext();
+  const { isOpen, triggerId, menuId, triggerRef, menuRef } = useDropdownContext();
+
+
+  if (isOpen) positionDropdown();
+
+  useFocusTrap(menuRef as React.RefObject<HTMLDivElement | null>, isOpen);
+
+  useEffect(() => {
+    if (!menuRef || !ref) return;
+    if (typeof ref === 'function') ref(menuRef.current);
+    else ref.current = menuRef.current;
+  }, [ref, isOpen]);
 
   if (!isOpen) return null;
   return createPortal(
     <div
       role="menu"
       id={menuId}
-      ref={ref}
+      ref={menuRef}
       className={`dropdown-content ${className}`}
       aria-labelledby={triggerId}
       {...props}
