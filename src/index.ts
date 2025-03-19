a836ad85cc9f980ddbabeb1b7b84d89319895949
jinew <jiyunk@naver.com>
2025-03-19 14:16:34 +0900

    add: 엔트리 포인트 index.ts 작성

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './global-style.css';
+
+export { default as Breadcrumb } from './components/breadcrumb';
+export { default as Dropdown } from './components/dropdown';
+export { default as Modal } from './components/modal';
+export { default as Tabs } from './components/tabs';
