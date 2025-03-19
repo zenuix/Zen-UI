411e3454dfc7fa64a89531f6243937ea86e64007
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-19 10:08:41 +0900

    fix:#29 tooltip 표시 위치 문제 해결 진행 중

--- src/components/tooltip/subcomponents/tooltip-content.tsx
+++ src/components/tooltip/subcomponents/tooltip-content.tsx
@@ -8,34 +8,11 @@ interface TooltipContentProps {
 }
 
 export function TooltipContent({ children, className }: TooltipContentProps) {
-  const { isOpen, position, animation, offset } = useTooltipContext();
+  const { isOpen, position, animation } = useTooltipContext();
 
   if (!isOpen) return null;
 
-  return (
-    <span
-      className={clsx(
-        'tooltipContent',
-        {
-          // 위치에 따른 조건부 클래스
-          top: position === 'top',
-          bottom: position === 'bottom',
-          left: position === 'left',
-          right: position === 'right',
-
-          // 애니메이션 타입에 따른 조건부 클래스
-          fade: animation === 'fade',
-          scale: animation === 'scale',
-
-          show: isOpen
-        },
-        className
-      )}
-      style={{ [`${position}`]: `calc(100% + ${offset}px)` }}
-    >
-      {children}
-    </span>
-  );
+  return <span className={clsx('tooltipContent', position, animation, { show: isOpen }, className)}>{children}</span>;
 }
 
 export default TooltipContent;
