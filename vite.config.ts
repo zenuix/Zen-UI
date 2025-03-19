1299aaf1f71ee6a06c37b86772798a691812daf7
jinew <jiyunk@naver.com>
2025-03-19 15:29:27 +0900

    chore : 배포 환경 재세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import * as path from 'path';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite';
+import { type PluginOption, defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json'
+    })
+  ] as PluginOption[],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -16,12 +25,14 @@ export default defineConfig({
       output: {
         globals: {
           react: 'React'
-        }
+        },
+        entryFileNames: 'index.js',
+        chunkFileNames: 'chunk-[name].js',
+        assetFileNames: 'index.[ext]'
       }
     },
     commonjsOptions: {
       esmExternals: ['react']
     }
-  },
-  plugins: [dts({ tsconfigPath: './tsconfig.app.json' })]
+  }
 });
