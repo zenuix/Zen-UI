ec4bd33c347b0bfab991b75a090c0e87ccadf354
jinew <jiyunk@naver.com>
2025-03-19 15:29:38 +0900

    fix: import style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import './global-style.css';
+import './components/modal/style.css';
+import './components/dropdown/style.css';
+import './components/breadcrumb/style.css';
+import './components/tabs/style.css';
 
 export { default as Breadcrumb } from './components/breadcrumb';
 export { default as Dropdown } from './components/dropdown';
