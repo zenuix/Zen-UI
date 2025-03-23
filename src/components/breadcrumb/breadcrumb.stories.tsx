5aca5d4b3085df426c62c96cbdc97cd104321aa1
hoki9797 <hoki9797@gmail.com>
2025-03-23 17:36:26 +0900

    feat#36: 스토리북 수정

--- src/components/breadcrumb/breadcrumb.stories.tsx
+++ src/components/breadcrumb/breadcrumb.stories.tsx
@@ -6,13 +6,47 @@ import { ArrowBigDownDash, ArrowBigRightDash } from 'lucide-react';
 
 export default {
   title: 'Components/Breadcrumb',
-  component: Breadcrumb
-} as Meta;
+  component: Breadcrumb,
+  tags: ['autodocs'],
+  argTypes: {
+    direction: {
+      control: 'radio',
+      options: ['row', 'col'],
+      description: 'Breadcrumb list direction',
+      table: {
+        defaultValue: { summary: 'row' }
+      }
+    },
+    maxItems: {
+      control: 'number',
+      description: 'Max number of breadcrumb items before collapsing'
+    },
+    itemsBeforeCollapse: {
+      control: 'number',
+      description: 'Number of items to show before collapse'
+    },
+    itemsAfterCollapse: {
+      control: 'number',
+      description: 'Number of items to show after collapse'
+    },
+    ellipsisStyle: {
+      control: false,
+      description: 'Custom ellipsis content for collapsed items (string or JSX element)'
+    }
+  },
+  args: {
+    direction: 'row',
+    maxItems: undefined,
+    itemsBeforeCollapse: 1,
+    itemsAfterCollapse: 2,
+    ellipsisStyle: '...'
+  }
+} as Meta<typeof Breadcrumb>;
 
-const DefaultTemplate: StoryFn = () => (
+const DefaultTemplate: StoryFn = (args) => (
   <>
     <Breadcrumb>
-      <Breadcrumb.list>
+      <Breadcrumb.list {...args} ellipsisStyle={<button onClick={() => alert('More!')}>👀 See All</button>}>
         <Breadcrumb.item>
           <Breadcrumb.link href="/">Home</Breadcrumb.link>
         </Breadcrumb.item>
@@ -30,10 +64,13 @@ const DefaultTemplate: StoryFn = () => (
 );
 
 export const Default = DefaultTemplate.bind({});
+Default.args = {
+  direction: 'row'
+};
 
-const DirectionTemplate: StoryFn = () => (
+const DirectionTemplate: StoryFn = (args) => (
   <Breadcrumb>
-    <Breadcrumb.list direction="col">
+    <Breadcrumb.list {...args}>
       <Breadcrumb.item>
         <Breadcrumb.link href="/">Home</Breadcrumb.link>
       </Breadcrumb.item>
@@ -54,10 +91,13 @@ const DirectionTemplate: StoryFn = () => (
 );
 
 export const Direction = DirectionTemplate.bind({});
+Direction.args = {
+  direction: 'col'
+};
 
-const CustomSeparatorTemplate: StoryFn = () => (
+const CustomSeparatorTemplate: StoryFn = (args) => (
   <Breadcrumb>
-    <Breadcrumb.list>
+    <Breadcrumb.list {...args}>
       <Breadcrumb.item>
         <Breadcrumb.link href="/">Home</Breadcrumb.link>
       </Breadcrumb.item>
@@ -79,14 +119,9 @@ const CustomSeparatorTemplate: StoryFn = () => (
 
 export const CustomSeparator = CustomSeparatorTemplate.bind({});
 
-const CollapseTemplate: StoryFn = () => (
+const CollapseAndCustomEllipsisTemplate: StoryFn = (args) => (
   <Breadcrumb>
-    <Breadcrumb.list
-      maxItems={5}
-      itemsBeforeCollapse={2}
-      itemsAfterCollapse={3}
-      ellipsisStyle={<span onClick={() => alert('Expand Breadcrumbs!')}>🔍 More...</span>}
-    >
+    <Breadcrumb.list {...args} ellipsisStyle={<span style={{ cursor: 'pointer' }}>🔍 More...</span>}>
       <Breadcrumb.item>
         <Breadcrumb.link href="/">Item 1</Breadcrumb.link>
       </Breadcrumb.item>
@@ -118,4 +153,9 @@ const CollapseTemplate: StoryFn = () => (
   </Breadcrumb>
 );
 
-export const Collapse = CollapseTemplate.bind({});
+export const CollapseAndCustomEllipsis = CollapseAndCustomEllipsisTemplate.bind({});
+CollapseAndCustomEllipsis.args = {
+  maxItems: 5,
+  itemsBeforeCollapse: 2,
+  itemsAfterCollapse: 3
+};
--- src/components/breadcrumb/breadcrumb.tsx
+++ src/components/breadcrumb/breadcrumb.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import './style.css';
-import '../../global-style.css';
 
 type BreadcrumbProps = React.HTMLAttributes<HTMLElement> & {};
 
